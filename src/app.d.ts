cfb92e1a9bfc3b5aa325d099201da33ea10309e4
barrystone <barrystone3183@gmail.com>
2022-08-25 09:03:41 +0800

    [clean] remove useless

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,19 +3,8 @@
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
 declare namespace App {
-	interface Locals {}
-
-	interface Platform {
-		env: {
-			COUNTER: DurableObjectNamespace;
-		};
-		context: {
-			waitUntil(promise: Promise<any>): void;
-		};
-		caches: CacheStorage & { default: Cache };
-	}
-
-	interface Session {}
-
-	interface Stuff {}
+	// interface Locals {}
+	// interface Platform {}
+	// interface Session {}
+	// interface Stuff {}
 }
